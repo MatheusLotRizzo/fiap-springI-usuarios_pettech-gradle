3297215b3f0aa6fc8f38664ba7cbbf9864acdad3
Matheus Lot Rizzo <matheus.l.rizzo@gmail.com>
2023-10-04 20:25:42 -0300

    first commit

---
+++ src/main/java/br/com/rizzo/usuarios_pettech/UsuariosPettechApplication.java
@@ -0,0 +1,13 @@
+package br.com.rizzo.usuarios_pettech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosPettechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsuariosPettechApplication.class, args);
+    }
+
+}
